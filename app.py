02cbd9666bb00bc430938ffb04f5ca624b7ab8a9
sukisuou <adamdiey3@gmail.com>
2026-03-05 14:12:31 +0800

    edited structure

--- app.py
+++ app.py
@@ -44,16 +44,18 @@ st.markdown("<h1 style='text-align: center;'>Diabetes Risk Prediction</h1>", uns
 # Prediction Interface
 st.header("Predict Risk")
 
-# User inputs from sidebar
-user_data = pd.DataFrame([get_user_input()])
-
-# Fix the missing data
-col_to_fix = [f'Feature{i}' for i in range (2, 7)]
-user_data[col_to_fix] = user_data[col_to_fix].replace(0, np.nan)
-user_data = user_data.fillna(medians)
 
 # Predict button
 if st.button("Predict"):
+    # User inputs from sidebar
+    user_data = pd.DataFrame([get_user_input()])
+
+    # Fix the missing data
+    col_to_fix = [f'Feature{i}' for i in range (2, 7)]
+    user_data[col_to_fix] = user_data[col_to_fix].replace(0, np.nan)
+    user_data = user_data.fillna(medians)
+
+    # Predict
     prediction = model.predict(user_data)
     st.subheader("Risk: ")
     risk = 'Diabetic' if prediction[0][0] > 0.5 else 'Not Diabetic'
